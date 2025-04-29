b7728d9b6b2409ff9ddec9f083c51a65a171520e
unknown <rezai.timur.04@gmail.com>
2025-04-29 12:10:30 -0700

    refactored step handling to account for non floor objects

--- game/game.cpp
+++ game/game.cpp
@@ -5,22 +5,34 @@
 #include <conio.h>
 using namespace std;
 
+//global
+char playerChar = 'T';
+char currTile = '_';
+
 //struct defs
 struct Pos {
     int row;
     int col;
 };
 
+struct Stats {
+    int stren = 1;
+    int craft = 1;
+};
+
 //func defs
 void displayRoom(char room[9][9], int size);
+void displayStats(Stats stats);
 void genRoom(char room[9][9], int size);
 void plotPlayer(char room[9][9], Pos& startPos, int size);
 void movePlayer(char room[9][9], Pos& pos, char move, int size);
+void stepHandler(char room[9][9], Pos& pos, char axis, int incr);
 
 //main func
 int main()
 {
     Pos pos;
+    Stats stats;
     char move;
     char room[9][9];
     
@@ -68,41 +80,52 @@ void plotPlayer(char room[9][9], Pos& startPos, int size) {
     srand(time(0));
     startPos;
     startPos.row = rand() % size, startPos.col = rand() % size;
-    room[startPos.row][startPos.col] = 'T';
+    room[startPos.row][startPos.col] = playerChar;
 }
 
 void movePlayer(char room[9][9], Pos& pos, char move, int size) {
     switch (move) {
     case 'w':
-        if ((pos.row - 1) >= 0 && (pos.row - 1) <= 8) {
-            room[pos.row][pos.col] = '_';
-            pos.row -= 1;
-            room[pos.row][pos.col] = 'T';
-        }
+        stepHandler(room, pos, 'r', -1);
         break;
     case 'a':
-        if ((pos.col - 1) >= 0 && (pos.col - 1) <= 8) {
-            room[pos.row][pos.col] = '_';
-            pos.col -= 1;
-            room[pos.row][pos.col] = 'T';
-        }
+        stepHandler(room, pos, 'c', -1);
         break;
     case 's':
-        if ((pos.row + 1) >= 0 && (pos.row + 1) <= 8) {
-            room[pos.row][pos.col] = '_';
-            pos.row += 1;
-            room[pos.row][pos.col] = 'T';
-        }
+        stepHandler(room, pos, 'r', 1);
         break;
     case 'd':
-        if ((pos.col + 1) >= 0 && (pos.col + 1) <= 8) {
-            room[pos.row][pos.col] = '_';
-            pos.col += 1;
-            room[pos.row][pos.col] = 'T';
-        }
+        stepHandler(room, pos, 'c', 1);
         break;
     default:
         cout << "wrong key use wasd for movement";
         break;
+    }
+}
+
+void displayStats(Stats stats) {
+    cout << "Strength: " << stats.stren << " Craft: " << stats.craft << endl;
+}
+
+void stepHandler(char room[9][9], Pos& pos, char axis, int incr) {
+    switch (axis) {
+    case 'r':
+        if ((pos.row + incr) >= 0 && (pos.row + incr) <= 8) {
+            room[pos.row][pos.col] = currTile;
+            currTile = room[pos.row + incr][pos.col];
+            pos.row += incr;
+            room[pos.row][pos.col] = playerChar;
+        }
+        break;
+    case 'c':
+        if ((pos.col + incr) >= 0 && (pos.col + incr) <= 8) {
+            room[pos.row][pos.col] = currTile;
+            currTile = room[pos.row][pos.col + incr];
+            pos.col += incr;
+            room[pos.row][pos.col] = playerChar;
+        }
+        break;
+    default:
+        cout << "error in step handling" << endl;
     }
 }
